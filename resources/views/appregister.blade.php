ed9185d09ee008650fddadf4e82d4768dad36920
ricotoribio <johnricotoribio@gmail.com>
2022-02-15 13:11:49 +0800

    zxc

--- resources/views/appregister.blade.php
+++ resources/views/appregister.blade.php
@@ -5,7 +5,8 @@
     <head>
         <meta charset="utf-8"/>
         <title>SDO RQA SYSTEM</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         
         <!-- Favicon -->
         <link rel="shortcut icon" href="{{asset('assets/images/sdo.png')}}">
