7ee8aceeb84c46c72ded50cf7aa4fd7f924081be
ricotoribio <johnricotoribio@gmail.com>
2022-04-04 15:09:11 +0800

    adf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                 width: 50%;
             }
             .center {
-                margin-top:3250px;
+                margin-top:-250px;
                 height: 1%;
                 display: flex;
                 align-items: center;
